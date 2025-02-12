9540fad221fce2a6ee6112179ec344c83ccdbdf9
yusuke-ten <net.escape.com@gmail.com>
2025-02-12 17:10:41 +0900

    modify eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,87 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js'
+import nextPlugin from '@next/eslint-plugin-next'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import perfectionist from 'eslint-plugin-perfectionist'
+import reactPlugin from 'eslint-plugin-react'
+import hooksPlugin from 'eslint-plugin-react-hooks'
+import unusedImports from 'eslint-plugin-unused-imports'
+import tseslint from 'typescript-eslint'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+export default tseslint.config(
+  {
+    files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+  },
+  {
+    ignores: [
+      '**/next-env.d.ts',
+      '**/.next/',
+      'prettier.config.js',
+    ],
+    name: 'Ignore',
+  },
+  {
+    name: 'eslint/recommended',
+    ...js.configs.recommended,
+  },
+  ...tseslint.configs.recommended,
+  {
+    name: 'Unused imports',
+    plugins: { 'unused-imports': unusedImports },
+    rules: {
+      'unused-imports/no-unused-imports': 1,
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+      'unused-imports/no-unused-vars': [
+        0,
+        {
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          vars: 'all',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    name: 'react/jsx-runtime',
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: reactPlugin.configs['jsx-runtime'].rules,
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    name: 'react-hooks/recommended',
+    plugins: {
+      "react-hooks": hooksPlugin,
+    },
+    rules: hooksPlugin.configs.recommended.rules,
+  },
+  {
+    name: 'next/core-web-vitals',
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    extends: [perfectionist.configs['recommended-natural']],
+    name: 'Perfectionist',
+  },
+  {
+    name: 'prettier/config',
+    ...eslintConfigPrettier,
+  },
+  {
+    name: 'project-custom',
+    rules: {
+      '@typescript-eslint/no-unused-vars': 1,
+    },
+  },
+)
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  jsxSingleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 90,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
